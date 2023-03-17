d017ca9d13c0d828502bf5cacd19812ce801b1fa
brett.green <brett.green@quidient.com>
2023-03-17 17:07:37 -0400

    [libintegrate] fix integration issues

--- ports/libintegrate/portfile.cmake
+++ ports/libintegrate/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     SHA512 c1956f33402b4202a4f7e23c237459837f4f043abd6a4f033198fbde65ab7ffe2c4e76a2d1da10ca98b2a3d9e88b00377078a86ae24ebd4f285fcd5c47a4fb8c
     HEAD_REF master
 )
-
+set(VCPKG_BUILD_TYPE release)
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
@@ -15,11 +15,9 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-file(INSTALL ${CURRENT_PACKAGES_DIR}/cmake/libIntegrateConfig.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(INSTALL ${CURRENT_PACKAGES_DIR}/cmake/libIntegrateConfigVersion.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(INSTALL ${CURRENT_PACKAGES_DIR}/cmake/libIntegrateTargets.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(INSTALL ${SOURCE_PATH}/LICENSE.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib ${CURRENT_PACKAGES_DIR}/cmake)
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/libIntegrate/libIntegrate/Experimental")
 
+file(INSTALL ${SOURCE_PATH}/LICENSE.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/libIntegrate/libIntegrate/Experimental")
+vcpkg_cmake_config_fixup(PACKAGE_NAME libIntegrate CONFIG_PATH cmake)
 vcpkg_fixup_pkgconfig()
